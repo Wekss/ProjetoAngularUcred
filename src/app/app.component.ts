9949f247db91947c916e8793d4e48e651e27371f
WeksleyAugustinhoLim <weksley17@hotmail.com>
2024-07-05 17:56:59 -0300

    shared.ts and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import {CommonModule} from "@angular/common";
 import {TemplateBindingComponent} from "./components/template-binding/template-binding.component";
 import {HeaderComponent} from "./components/header/header.component";
 import {ListComponent} from "./components/list/list.component";
+import { SharedModule } from './shared/shared.module';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent,TemplateBindingComponent,ListComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent,TemplateBindingComponent,ListComponent,SharedModule],
 
   template: `
 
