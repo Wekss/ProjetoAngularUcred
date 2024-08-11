b07cdccc9e290a3d3f8102699c2cbd61594ea483
Weks <weksley17@hotmail.com>
2024-08-11 19:11:16 -0300

    Pic local storage

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -17,5 +17,32 @@ import {MatIconModule} from '@angular/material/icon';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  handleFileInput(event: any) {
+    const files = event.target.files;
+    if (files.length === 0) {
+      return; // Nenhum arquivo selecionado
+    }
 
+    const reader = new FileReader();
+    const images: string[] = [];
+    let filesRead = 0;
+
+    // Função para ler um arquivo e armazenar a imagem
+    const readFile = (file: File) => {
+      reader.onloadend = () => {
+        images.push(reader.result as string);
+        filesRead++;
+        if (filesRead === files.length) {
+          // Armazena todas as imagens em Base64 no localStorage
+          localStorage.setItem('uploadedImages', JSON.stringify(images));
+        }
+      };
+      reader.readAsDataURL(file);
+    };
+
+    // Itera sobre os arquivos selecionados e os lê
+    for (let i = 0; i < files.length; i++) {
+      readFile(files[i]);
+    }
+  }
 }
